7988a9c2289b308c8b67edab5c9b82cc24a5a3f8
Kyle Halog (IS-PH) <kyle_halog@trendmicro.com>
2017-11-29 22:42:02 +0800

    add routes that will return some of the html files

--- main.py
+++ main.py
@@ -32,6 +32,9 @@ def register():
         print(response.text)
     return render_template('register.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('/dashboard.html')
 
 if __name__ == '__main__':
     app.run(port=8080)
