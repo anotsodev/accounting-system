127e0e41e5e4193e78cc79405f763741f94e8113
Kyle Halog (IS-PH) <kyle_halog@trendmicro.com>
2017-11-30 18:14:56 +0800

    login partially working

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
-    
+    /*
+      Register Section
+    */ 
     $( "#register" ).click(function() {
       var url = "http://10.5.92.201:5000"
 
@@ -22,16 +24,60 @@ $( document ).ready(function() {
                 window.location.href = '/login';
             },
             error: function (request, message, error) {
-              handler_error(request.responseText);
+              fields_error_handler(request.responseText);
             }
           }
             $.ajax(settings);
     });
+    /*
+      Register Section
+    */
+    $( "#login" ).click(function() {
+      var url = "http://10.5.92.201:5000"
+      var data = JSON.stringify($('form').serializeObject())
+      var response = {};
+      var auth = 
+      parsed_data = JSON.parse(data)
+      var credential = parsed_data['username']+":"+parsed_data['password'];
+      auth = encode_base64(credential)
+      var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": url+"/users/login",
+            "method": "POST",
+            "headers": {
+              "accept": "application/json",
+              "content-type": "application/json",
+              "cache-control": "no-cache",
+              "authorization": auth
+            },
+            "dataType": "json",
+            success: function () {
+                window.location.href = '/dashboard';
+            },
+            error: function (request, message, error) {
+              error_message_handler(request.responseText);
+            }
+          }
+            $.ajax(settings);
+    }); 
 
 });
 
+function encode_base64(string) {
+  // Create Base64 Object
+      var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
 
-function handler_error(error_message){
+      // Encode the String
+      var encodedString = Base64.encode(string);
+      return encodedString
+}
+
+function error_message_handler(error_message){
+  $( "#error-message" ).html(JSON.parse(error_message)['message'])
+}
+
+function fields_error_handler(error_message){
   $( "#error-message" ).html(function(){
     var ret = "<p>Please check the following fields:</p>"
     error_data = JSON.parse(error_message);
